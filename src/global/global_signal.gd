98e80a8af4b8cfbc5ba6182b5b3853a1cdd88530
Jonathan William Kurniawan <jonathan.kurniawan@binus.edu>
2023-12-10 03:16:32 +0700

    add password ui

--- src/global/global_signal.gd
+++ src/global/global_signal.gd
@@ -5,4 +5,5 @@ signal arena_finished
 signal arena_entered(arena: Node2D)
 signal iteration_changed(iteration: int)
 signal broadcast_player(player: Player)
+signal correct_password
 signal switch_triggered
