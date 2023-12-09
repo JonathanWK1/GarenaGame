93df9519161ab3c30fec438fc59c7c74f4d07eb6
Jonathan William Kurniawan <jonathan.kurniawan@binus.edu>
2023-12-09 14:45:21 +0700

    add smoothing, temp tile map

--- src/speaker/speaker.gd
+++ src/speaker/speaker.gd
@@ -2,7 +2,15 @@ extends Node2D
 
 
 @export var to_show: Control
+@export var text: String
 
+@onready var label = $ColorRect/Label
+
+func _ready():
+	label.text = text;
+
+func set_text(value : String):
+	label.text = value;
 
 func _on_trigger_area_area_entered(area: Area2D) -> void:
 	to_show.show()
