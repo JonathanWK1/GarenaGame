18b4d25ed2fa4fb34c0b8b76f2408e69778799b1
Insta-x <uttsadajason@outlook.com>
2023-12-10 10:30:20 +0700

    fix special ram

--- src/enemy/ram/special_ram_enemy.gd
+++ src/enemy/ram/special_ram_enemy.gd
@@ -1,5 +1,19 @@
 extends RamEnemy
 
 
+@onready var special_collision: CollisionShape2D = $SpecialArea/CollisionShape2D
+
+
+func _on_ram_state_entered() -> void:
+	super._on_ram_state_entered()
+	special_collision.set_deferred('disabled', false)
+
+
+func _on_ram_state_exited() -> void:
+	super._on_ram_state_exited()
+	special_collision.set_deferred('disabled', true)
+
+
 func _on_special_area_area_entered(area: Area2D) -> void:
+	collided_audio.play()
 	state_chart.send_event('ram_collided')
